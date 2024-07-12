c0fff496b66891c798ef3dfa340da6bd87924690
Katya Greimes <katyagreimes@gmail.com>
2024-07-12 12:00:46 -0700

    Fonts

--- castle-magic-react/castle-magic/src/Components/Hero.js
+++ castle-magic-react/castle-magic/src/Components/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
             <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', height: 800, width: "100%" }}>
               <Container className='d-flex justify-content-center align-items-center h-100'>
                 <div className='text-light'>
-                  <h1 className='mb-3 display-2 fw-bold'>CastleMagic</h1>
+                  <h1 className='mb-3 display-2 fw-bold'>CastleMagic Castle Builders</h1>
                   <h4 className='mb-3'>A source for building medieval style stone castles, plans, designs, and castles for sale</h4>
                   <Link to="/faq">
                     <Button className='btn-light btn-lg m-2' >
--- castle-magic-react/castle-magic/src/features/castles/CASTLES.js
+++ castle-magic-react/castle-magic/src/features/castles/CASTLES.js
@@ -1,34 +1,35 @@
 // These imports are pictures
-import kataryna from "../../app/assets/kataryna.jpg"; 
+import kataryna from "../../app/assets/kataryna.jpg";
 import lakeCastle from "../../app/assets/lakecastle.jpg";
 import historyTower from "../../app/assets/History2.png";
 
 // Don't forget the curly braces and commas!
 
 export const CASTLES = [
-    {
-        id: 0,
-        name: "Castle Kataryna",
-        image: kataryna,
-        location: "Schweitzer Mountain",
-        blurb: "Castle on mountain.",
-        description: "Today's modern architecture and construction are now based on reverse building. As an example, today a home builder most often builds the house to the roof first and then glues imitation stone onto the building to attempt to give the illusion that the structure is strong as stone. However, the real and correct method is to stack the stones first and then build the house or roof on top of the strong base. Adding imitation stone work after the house is built or framed weakens and puts stress on the structure instead of adding strength."
-
-    },
-    {
-        id: 1,
-        name: "Castle Von Frandsen",
-        image: lakeCastle,
-        location: "Lake Pend Oreille",
-        blurb: "Castle on the Lake.",
-        description: "Today's modern architecture and construction are now based on reverse building. As an example, today a home builder most often builds the house to the roof first and then glues imitation stone onto the building to attempt to give the illusion that the structure is strong as stone. However, the real and correct method is to stack the stones first and then build the house or roof on top of the strong base. Adding imitation stone work after the house is built or framed weakens and puts stress on the structure instead of adding strength."
-    },
-    {
-        id: 2,
-        name: "History Channel Tower",
-        image: historyTower,
-        location: "Lake Pend Oreille",
-        blurb: "Built for The Big Build.",
-        description: "Long description of everything about this Tower"
-    },
+  {
+    id: 0,
+    name: "Castle Kataryna",
+    image: kataryna,
+    location: "Schweitzer Mountain",
+    blurb: "Castle on mountain.",
+    description:
+      "Today's modern architecture and construction are now based on reverse building. As an example, today a home builder most often builds the house to the roof first and then glues imitation stone onto the building to attempt to give the illusion that the structure is strong as stone. However, the real and correct method is to stack the stones first and then build the house or roof on top of the strong base. Adding imitation stone work after the house is built or framed weakens and puts stress on the structure instead of adding strength.",
+  },
+  {
+    id: 1,
+    name: "Castle Von Frandsen",
+    image: lakeCastle,
+    location: "Lake Pend Oreille",
+    blurb: "Castle on the Lake.",
+    description:
+      "Today's modern architecture and construction are now based on reverse building. As an example, today a home builder most often builds the house to the roof first and then glues imitation stone onto the building to attempt to give the illusion that the structure is strong as stone. However, the real and correct method is to stack the stones first and then build the house or roof on top of the strong base. Adding imitation stone work after the house is built or framed weakens and puts stress on the structure instead of adding strength.",
+  },
+  {
+    id: 2,
+    name: "History Channel Tower",
+    image: historyTower,
+    location: "Lake Pend Oreille",
+    blurb: "Built for The Big Build.",
+    description: "Long description of everything about this Tower",
+  },
 ];
